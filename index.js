72c24a1622471684beb5d2f3f2b15857b0af5780
erudeck <erudeck@deeplydigital.co.uk>
2025-04-10 23:00:33 +0100

    Add Home, And API Environment, make some fixes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,9 +9,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+
+app.get('/', async (req, res) => {
+    const holidays = 'https://api.hubspot.com/crm/v3/objects/2-141377370?properties=name&properties=duration&properties=location';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(holidays, { headers });
+        const data = resp.data.results;
+        // console.log(data);
+        res.render('homepage', { title: 'Custom Object Table | Integrating With HubSpot I Practicum', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
@@ -31,7 +48,7 @@ app.get('/update-cobj', async (req, res) => {
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-app.post('/update-obj', async (req, res) => {
+app.post('/update-cobj', async (req, res) => {
     const data = {
         properties: {
             "name": req.body.name,
@@ -40,7 +57,8 @@ app.post('/update-obj', async (req, res) => {
         }
     }
 
-    const createObject = `https://api.hubapi.com/crm/v3/objects/2-19410066`;
+    console.log(data);
+    const createObject = `https://api.hubapi.com/crm/v3/objects/2-141377370`;
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
